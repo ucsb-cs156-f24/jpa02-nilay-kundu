94f1de8cc75b6beef0b5bef7dbd610b1c183d661
Nilay Kundu <nilaykundu@ucsb.edu>
2024-10-09 13:07:30 -0700

    Update Team tests for equals mutation line and mutation coverage

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -22,5 +22,36 @@ public class TeamTest {
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    @Test
+    public void equals_compares_objects_correctly() {
+        // Case 1: Same object
+        assertEquals(true, team.equals(team));
+
+        // Case 2: Diff class
+        String test = "test";
+        assertEquals(false, team.equals(test));
+
+        // Case 3: Diff Team objects --> 4 sub-cases
+        Team test_same_name = new Team("test-team");
+        Team test_wrong_name = new Team("test-wrong-team");
+
+        // Compares same name and same members: true
+        assertEquals(true, team.equals(test_same_name));
+
+        // Compares same name but diff members: false
+        test_same_name.addMember("Mary Jane");
+        assertEquals(false, team.equals(test_same_name));
+        
+        // Compares diff name but same members: false
+        assertEquals(false, team.equals(test_wrong_name));
+
+        // Compares diff name and diff members: false
+        test_wrong_name.addMember("Mary Jane");
+        assertEquals(false, team.equals(test_wrong_name));
+    }
 }
